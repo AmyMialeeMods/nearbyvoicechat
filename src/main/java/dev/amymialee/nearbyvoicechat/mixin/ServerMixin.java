6daff690a156d397809fac0f8ae2056f9976a742
AmyMialee <10999535+AmyMialee@users.noreply.github.com>
2026-01-29 16:26:31 +0000

    mojmap

--- src/main/java/dev/amymialee/nearbyvoicechat/mixin/ServerMixin.java
+++ src/main/java/dev/amymialee/nearbyvoicechat/mixin/ServerMixin.java
@@ -9,7 +9,7 @@ import de.maxhenkel.voicechat.Voicechat;
 import de.maxhenkel.voicechat.plugins.PluginManager;
 import de.maxhenkel.voicechat.voice.common.*;
 import de.maxhenkel.voicechat.voice.server.*;
-import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.server.level.ServerPlayer;
 import org.jspecify.annotations.NonNull;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -25,26 +25,26 @@ public abstract class ServerMixin {
 
     @Inject(method = "processGroupPacket", at = @At(value = "HEAD"))
     private void nearby$init(
-            PlayerState senderState, ServerPlayerEntity sender, @NonNull MicPacket packet, CallbackInfo ci,
-            @Share("distance") LocalFloatRef distance, @Share("nearby") LocalRef<Collection<ServerPlayerEntity>> nearby) {
+            PlayerState senderState, ServerPlayer sender, @NonNull MicPacket packet, CallbackInfo ci,
+            @Share("distance") LocalFloatRef distance, @Share("nearby") LocalRef<Collection<ServerPlayer>> nearby) {
         if (packet.isWhispering()) {
             distance.set(Voicechat.SERVER_CONFIG.whisperDistance.get().floatValue());
         } else {
             distance.set(Utils.getDefaultDistanceServer());
         }
-        distance.set(PluginManager.instance().getDistance(sender, senderState, packet, distance.get()) - 2);
-        nearby.set(ServerWorldUtils.getPlayersInRange(sender.getEntityWorld(), sender.getEntityPos(), this.getBroadcastRange(distance.get()), (p) -> !p.getUuid().equals(sender.getUuid())));
+        distance.set(PluginManager.instance().getDistance(sender, senderState, packet, distance.get()) * (0.75f));
+        nearby.set(ServerWorldUtils.getPlayersInRange(sender.level(), sender.position(), this.getBroadcastRange(distance.get()), (p) -> !p.getUUID().equals(sender.getUUID())));
     }
 
-    @WrapOperation(method = "processGroupPacket", at = @At(value = "INVOKE", target = "Lde/maxhenkel/voicechat/voice/server/Server;sendSoundPacket(Lnet/minecraft/server/network/ServerPlayerEntity;Lde/maxhenkel/voicechat/voice/common/PlayerState;Lnet/minecraft/server/network/ServerPlayerEntity;Lde/maxhenkel/voicechat/voice/common/PlayerState;Lde/maxhenkel/voicechat/voice/server/ClientConnection;Lde/maxhenkel/voicechat/voice/common/SoundPacket;Ljava/lang/String;)V"))
+    @WrapOperation(method = "processGroupPacket", at = @At(value = "INVOKE", target = "Lde/maxhenkel/voicechat/voice/server/Server;sendSoundPacket(Lnet/minecraft/server/level/ServerPlayer;Lde/maxhenkel/voicechat/voice/common/PlayerState;Lnet/minecraft/server/level/ServerPlayer;Lde/maxhenkel/voicechat/voice/common/PlayerState;Lde/maxhenkel/voicechat/voice/server/ClientConnection;Lde/maxhenkel/voicechat/voice/common/SoundPacket;Ljava/lang/String;)V"))
     private void nearby$pivot(
-            Server instance, @NonNull ServerPlayerEntity sender, @NonNull PlayerState senderState,
-            ServerPlayerEntity receiver, PlayerState receiverState, ClientConnection connection,
+            Server instance, @NonNull ServerPlayer sender, @NonNull PlayerState senderState,
+            ServerPlayer receiver, PlayerState receiverState, ClientConnection connection,
             SoundPacket<?> soundPacket, String source, Operation<Void> original,
-            PlayerState senderState2, ServerPlayerEntity sender2, @NonNull MicPacket packet,
-            @Share("distance") LocalFloatRef distance, @Share("nearby") LocalRef<Collection<ServerPlayerEntity>> nearby) {
+            PlayerState senderState2, ServerPlayer sender2, @NonNull MicPacket packet,
+            @Share("distance") LocalFloatRef distance, @Share("nearby") LocalRef<Collection<ServerPlayer>> nearby) {
         if (!sender.isSpectator() && nearby.get().contains(receiver)) {
-            soundPacket = new PlayerSoundPacket(sender.getUuid(), sender.getUuid(), packet.getData(), packet.getSequenceNumber(), packet.isWhispering(), distance.get(), null);
+            soundPacket = new PlayerSoundPacket(sender.getUUID(), sender.getUUID(), packet.getData(), packet.getSequenceNumber(), packet.isWhispering(), distance.get(), null);
             source = "proximity";
         }
         original.call(instance, sender, senderState, receiver, receiverState, connection, soundPacket, source);
